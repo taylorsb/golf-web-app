e91c25f0e2590d3135a183f6f47b147d0d58dc7a
Simon <simon_james_taylor@hotmail.com>
2025-08-12 21:52:00 +0100

    Trigger React frontend deployment with App.js change (attempt 2)

--- golfapp-client/src/App.js
+++ golfapp-client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; // Trigger build - second time
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainMenu from './MainMenu';
