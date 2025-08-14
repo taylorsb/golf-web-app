4aa3339507464b641c32c6fcbe807a163bbc6050
Simon <simon_james_taylor@hotmail.com>
2025-08-14 17:14:26 +0100

    Non-functional change to trigger GitHub Action (2)

--- golfapp-server/app.py
+++ golfapp-server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-# Non-functional change to trigger workflow
+# Another non-functional change to trigger workflow
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate # Import Migrate
