5451292017851c9685e5e7b072d36fa3d04ab563
Simon <simon_james_taylor@hotmail.com>
2025-08-14 18:36:22 +0100

    Fix: Revert main.yml DATABASE_URL and use connect_args for SSL in env.py (again) & Non-functional change

--- golfapp-server/app.py
+++ golfapp-server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-# Another non-functional change to trigger workflow (8)
+# Another non-functional change to trigger workflow (9)
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate # Import Migrate
