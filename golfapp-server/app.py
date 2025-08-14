632224f7960c81df7448822d873958b4bc0c6cd5
Simon <simon_james_taylor@hotmail.com>
2025-08-14 17:33:34 +0100

    Fix: Try charset=utf8 in GA & Non-functional change

--- golfapp-server/app.py
+++ golfapp-server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-# Another non-functional change to trigger workflow (3)
+# Another non-functional change to trigger workflow (4)
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate # Import Migrate
