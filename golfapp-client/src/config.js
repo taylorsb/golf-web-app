855e185c5de6a1a4abc087af62a99ab91e1b3c8b
Simon <simon_james_taylor@hotmail.com>
2025-09-05 21:00:54 +0100

    Fix: Use main application URL for API

--- golfapp-client/src/config.js
+++ golfapp-client/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://golf-app--0000006.greensky-eadbd98c.uksouth.azurecontainerapps.io' 
+    ? 'https://golf-app.greensky-eadbd98c.uksouth.azurecontainerapps.io' 
     : 'http://127.0.0.1:5000';
 
 export default API_URL;
